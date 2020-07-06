a4aaca9e2df175779e9a76d514e3da063c55faae
malk1405 <malk1405@mail.ru>
2020-07-06 23:24:23 +0300

    Удалил ненужные зависимости

--- js/main.js
+++ js/main.js
@@ -514,13 +514,13 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");
 /* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_dom__WEBPACK_IMPORTED_MODULE_1__);
-/* harmony import */ var _containers_app_app__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./containers/app/app */ "./source/js/containers/app/app.js");
-/* harmony import */ var promise_polyfill_src_polyfill__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! promise-polyfill/src/polyfill */ "./node_modules/promise-polyfill/src/polyfill.js");
+/* harmony import */ var promise_polyfill_src_polyfill__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! promise-polyfill/src/polyfill */ "./node_modules/promise-polyfill/src/polyfill.js");
+/* harmony import */ var _containers_app_app__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./containers/app/app */ "./source/js/containers/app/app.js");
 
 
 
 
-react_dom__WEBPACK_IMPORTED_MODULE_1___default.a.render( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_containers_app_app__WEBPACK_IMPORTED_MODULE_2__["default"], null), document.querySelector("#root"));
+react_dom__WEBPACK_IMPORTED_MODULE_1___default.a.render( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_containers_app_app__WEBPACK_IMPORTED_MODULE_3__["default"], null), document.querySelector("#root"));
 
 /***/ }),
 
