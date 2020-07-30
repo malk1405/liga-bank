ed47f25a3e868d3ffe77954a0f98bdd8a697031a
malk1405 <malk1405@mail.ru>
2020-07-30 06:23:13 +0300

    Доделал функционал модального окна

--- source/js/components/login/form.js
+++ source/js/components/login/form.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import getClasses from '../../utils/getClasses';
+import Logo from '../../../img/svg/inline/logo-ext.svg';
+import Icon from '../../../img/svg/inline/show.svg';
 
 const block = `login`;
 
@@ -37,63 +39,72 @@ function LoginForm({onSubmit, inputRef}) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className={getClasses({block, element: `fields`})}>
-        <label className={getClasses({block, element: `label`})}>
-          <span className={getClasses({block, element: `label-text`})}>
-            Логин
-          </span>
-          <input
-            className={`input ${getClasses({block, element: `field`})}`}
-            ref={inputRef}
-            type="text"
-            name="login"
-            value={fields.login || ``}
-            required
-            onChange={onChange}
-            minLength={3}
-            maxLength={40}
-          />
-        </label>
-        <div>
-          <label
-            className={getClasses({
-              block,
-              element: `label`,
-              modifiers: [`phone`],
-            })}
-          >
+    <div>
+      <Logo />
+      <form onSubmit={handleSubmit}>
+        <div className={getClasses({block, element: `fields`})}>
+          <label className={getClasses({block, element: `label`})}>
             <span className={getClasses({block, element: `label-text`})}>
-              Пароль
+              Логин
             </span>
             <input
               className={`input ${getClasses({block, element: `field`})}`}
-              type={isShown ? `text` : `password`}
-              name="password"
-              onChange={onChange}
-              value={fields.password || ``}
+              ref={inputRef}
+              type="text"
+              name="login"
+              value={fields.login || ``}
               required
-              minLength={6}
+              onChange={onChange}
+              minLength={3}
               maxLength={40}
             />
-            <button
-              type="button"
-              title="Показать пароль"
-              onMouseDown={showPassword}
-              onMouseLeave={hidePassword}
-              onMouseUp={hidePassword}
-            ></button>
           </label>
+          <div>
+            <label
+              className={getClasses({
+                block,
+                element: `label`,
+                modifiers: [`phone`],
+              })}
+            >
+              <span className={getClasses({block, element: `label-text`})}>
+                Пароль
+              </span>
+              <input
+                className={`input ${getClasses({block, element: `field`})}`}
+                type={isShown ? `text` : `password`}
+                name="password"
+                onChange={onChange}
+                value={fields.password || ``}
+                required
+                minLength={6}
+                maxLength={40}
+              />
+              <button
+                type="button"
+                title="Показать пароль"
+                className="button"
+                onKeyDown={showPassword}
+                onKeyUp={hidePassword}
+                onBlur={hidePassword}
+                onMouseDown={showPassword}
+                onMouseLeave={hidePassword}
+                onMouseUp={hidePassword}
+              ><Icon /></button>
+            </label>
+          </div>
         </div>
-      </div>
-      <button
-        type="submit"
-        className={getClasses({block: `button`, modifiers: [`main`]})}
-      >
-        Войти
-      </button>
-      <a href="#" className="link">Забыли пароль?</a>
-    </form>
+        <button
+          type="submit"
+          className={getClasses({block: `button`, modifiers: [`main`]})}
+        >
+          Войти
+        </button>
+        <a href="#" className="link">
+          Забыли пароль?
+        </a>
+      </form>
+    </div>
   );
 }
 
