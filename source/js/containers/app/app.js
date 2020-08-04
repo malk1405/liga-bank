5bff8213a241ca035ac01023213ad699b47ef797
malk1405 <malk1405@mail.ru>
2020-08-04 20:40:16 +0300

    Пустой проект

--- source/js/containers/app/app.js
+++ source/js/containers/app/app.js
@@ -1,47 +1,7 @@
-import React, {useState, useEffect} from 'react';
-
-import Header from '../../components/header/header';
-import Home from '../pages/home/home';
-import MediaContext from '../../context/media';
-import Footer from '../../components/footer/footer';
-
-const sizes = {phone: 767, tablet: 1023};
-const queries = Object.keys(sizes).map((el) => ({
-  type: `is${el.charAt(0).toUpperCase() + el.slice(1)}`,
-  query: `(max-width: ${sizes[el]}px)`,
-}));
+import React from 'react';
 
 function App() {
-  const [media, setMedia] = useState({});
-
-  useEffect(() => {
-    queries.forEach(({type, query}) => {
-      const mql = window.matchMedia(query);
-      mql.addListener(changeMedia);
-
-      changeMedia();
-
-      function changeMedia() {
-        setMedia((prevMedia) => {
-          return {...prevMedia, [type]: mql.matches};
-        });
-      }
-    });
-  }, []);
-
-  return (
-    <MediaContext.Provider value={media}>
-      <React.Fragment>
-        {/* Для отладки */}
-        <div className="body__bg"></div>
-        {/* Для отладки. Конец */}
-
-        <Header />
-        <Home />
-        <Footer />
-      </React.Fragment>
-    </MediaContext.Provider>
-  );
+  return <h1>Hello, world</h1>;
 }
 
 export default App;
